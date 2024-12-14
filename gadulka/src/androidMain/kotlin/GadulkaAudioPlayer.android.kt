dca0e810901316f76b3bd5e8a96e165d1ae39511
Konstantin <konstantin@headbright.be>
2024-12-14 15:03:38 +0100

    feat(#3): Document how to resolve resources on Android, add a convinience method for playing by resource id

--- gadulka/src/androidMain/kotlin/GadulkaAudioPlayer.android.kt
+++ gadulka/src/androidMain/kotlin/GadulkaAudioPlayer.android.kt
@@ -1,34 +1,13 @@
 package eu.iamkonstantin.kotlin.gadulka
 
+import android.content.ContentResolver
 import android.media.MediaPlayer
 
 import android.content.Context
+import android.net.Uri
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 
-//
-//actual class AudioPlayer(private val context: Context) {
-//
-//    private val mediaPlayer = ExoPlayer.Builder(context).build()
-//    private val mediaItems = soundResList.map {
-//        MediaItem.fromUri(Res.getUri(it))
-//    }
-//
-//    init {
-//        mediaPlayer.prepare()
-//    }
-//
-//    @OptIn(ExperimentalResourceApi::class)
-//    actual fun playSound(id: Int) {
-//        mediaPlayer.setMediaItem(mediaItems[id])
-//        mediaPlayer.play()
-//    }
-//
-//    actual fun release() {
-//        mediaPlayer.release()
-//    }
-//}
-//
 actual class GadulkaPlayer(private val context: Context) {
     private val mediaPlayer = ExoPlayer.Builder(context).build()
 
@@ -43,7 +22,20 @@ actual class GadulkaPlayer(private val context: Context) {
         mediaPlayer.play()
     }
 
+
+    /**
+     * Android-specific implementation of the [play] method which uses a ContentResolver to calculate the Uri of a raw file resource bundled with the app.
+     */
+    fun play(rawResourceId: Int) {
+        val uri = Uri.Builder()
+            .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+            .appendPath("$rawResourceId")
+            .build().toString()
+        play(uri)
+    }
+
     actual fun release() {
+        mediaPlayer.pause()
         mediaPlayer.release()
     }
 
--- gadulka/src/commonMain/kotlin/GadulkaAudioPlayer.kt
+++ gadulka/src/commonMain/kotlin/GadulkaAudioPlayer.kt
@@ -1,9 +1,50 @@
 package eu.iamkonstantin.kotlin.gadulka
 
+/**
+ * A minimalistic audio player
+ *
+ *
+ * Example:
+ *
+ * ```kotlin
+val player = GadulkaPlayer()
+player.play(url = "...")
+player.stop()
+player.release()
+```
+ */
 expect class GadulkaPlayer {
+    /**
+     * Start playback of the audio resource at the provided [url].
+     *
+     * ### Resource URI
+     *
+     * Can be a remote HTTP(s) url, or a `files` URI obtained via `Res.getUri("files/sample.mp3")`.
+     *
+     * Check the [JetBrains docs on how to store raw](https://www.jetbrains.com/help/kotlin-multiplatform-dev/compose-multiplatform-resources-usage.html#raw-files) files as part of multiplatform project resources.
+     *
+     * On Android, you can resolve the resource URI using something like:
+     *
+     * ```kotlin
+     *  val uri = Uri.Builder()
+                .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+                .appendPath("${R.raw.name_of_your_resource}")
+                .build().toString()
+     * ```
+     *
+     */
     fun play(url: String)
 
+    /**
+     * Stop playback.
+     *
+     * Note: If the player is currently not playing, this action has no effect.
+     */
     fun stop()
-    
+
+    /**
+     * Pause and attempts to perform cleanup in order to dispose of any player resources.
+     *
+     */
     fun release()
 }
